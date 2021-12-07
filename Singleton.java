852d15114285ed0347653148b0a97e3487efb688
Marcellino Adam <83111818+Marcellinoadam@users.noreply.github.com>
2021-12-07 19:04:23 +0700

    Add files via upload

---
+++ Singleton.java
@@ -0,0 +1,17 @@
+public class Singleton
+{
+  private static Singleton instance;
+	
+  private Singleton(){}
+	
+  public static Singleton GetInstance()
+  {
+    if(instance==null)
+      instance=new Singleton();
+	
+    return instance;
+  }
+  public void showMessage(){
+      System.out.println("Singleton pattern - Kelompok 7");
+   }
+}
